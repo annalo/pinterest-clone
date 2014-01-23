c4aa18d47400b12b248e01a379a47022098f15d1
Anna Lo <anna.yy.lo@gmail.com>
2014-01-22 21:35:55 -0800

    remove slug column from users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140122030435) do
+ActiveRecord::Schema.define(:version => 20140123053454) do
 
   create_table "boards", :force => true do |t|
     t.string   "name",        :null => false
@@ -72,11 +72,9 @@ ActiveRecord::Schema.define(:version => 20140122030435) do
     t.string   "session_token",   :null => false
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
-    t.string   "slug"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["session_token"], :name => "index_users_on_session_token", :unique => true
-  add_index "users", ["slug"], :name => "index_users_on_slug", :unique => true
 
 end
