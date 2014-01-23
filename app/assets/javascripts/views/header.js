bea025dd92da7bddcb3c10a025571098c9332af8
Anna Lo <anna.yy.lo@gmail.com>
2014-01-23 02:14:55 -0800

    browse other users boards

---
+++ app/assets/javascripts/views/header.js
@@ -0,0 +1,55 @@
+PinterestClone.Views.Header = Backbone.View.extend({
+	initialize: function() {
+		this.collection.bind("reset", this.render, this);
+	},
+
+	events: {
+		"click #new-pin": "newPin",
+		"click #new-board": "newBoard",
+		"keyup #search-form": "search"
+	},
+
+	template: JST["header"],
+
+	render: function() {
+		var renderedContent = this.template({ user: this.model });
+		this.$el.html(renderedContent);
+		return this;
+	},
+
+  newBoard: function() {
+    event.preventDefault();
+    var newBoard = new PinterestClone.Models.Board();    
+    var view = new PinterestClone.Views.BoardForm({ model: newBoard });
+    
+    $("#modal-body").empty();
+    $("#modal-body").append(view.render().$el);
+    $("#modal").modal("toggle");
+  },
+
+  newPin: function() {
+    event.preventDefault();
+    var type = $(event.target).data("type");
+    var newPin = new PinterestClone.Models.Pin();
+
+    if(type === "web") {
+      var view = new PinterestClone.Views.PinWeb({ model: newPin });
+    } else if(type === "upload") {
+      var view = new PinterestClone.Views.PinUpload({ model: newPin });
+    }
+
+    $("#modal-body").empty();
+    $("#modal-body").append(view.render().$el);
+    $("#modal").modal("toggle");
+  },
+
+  search: function(event) {
+  	var letters = $("#search-form").val();
+  	this.renderPins(this.collection.search(letters));
+  },
+
+  renderPins: function(pins) {
+  	var view = new PinterestClone.Views.PinsIndex({ collection: pins });
+  	$("#content").html(view.render().$el);
+  }
+});
